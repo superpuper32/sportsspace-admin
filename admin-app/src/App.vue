2ebf877c0ad00f532667657fa3ee53f76ab9a493
superpuper32 <pochitalin_m@mail.ru>
2020-05-27 23:17:52 +0300

    top bar navigation panel added

--- admin-app/src/App.vue
+++ admin-app/src/App.vue
@@ -34,7 +34,62 @@
         </ul>
       </aside>
 
-      <section class="content"></section>
+      <section class="content">
+        <nav class="top-bar">
+          <div class="top-bar__menu">
+            <div>
+              <button>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M3 18H21V16H3V18ZM3 13H21V11H3V13ZM3 6V8H21V6H3Z"
+                    fill="#98A9BC"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div>
+              <button>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M13 7H11V11H7V13H11V17H13V13H17V11H13V7ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z"
+                    fill="#98A9BC"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div>
+              <h3>Календарь</h3>
+            </div>
+          </div>
+
+          <div class="top-bar__settings">
+            <div class="top-bar__avatar">
+              <div class="top-bar__img">
+                <img
+                  src="https://www.gravatar.com/avatar/e3c4bc11e6b5852d8709c907104ef048?s=140&amp;d=retro"
+                  alt="Avatar"
+                />
+              </div>
+            </div>
+          </div>
+        </nav>
+      </section>
     </main>
     <div class="admin">
       <div class="admin__panel">
