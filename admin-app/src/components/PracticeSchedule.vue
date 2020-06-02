95c29e98461e0321da0ad5f1c0fc7ea8f7c1791a
superpuper32 <pochitalin_m@mail.ru>
2020-06-02 23:48:41 +0300

    calendar schedule fixed

--- admin-app/src/components/PracticeSchedule.vue
+++ admin-app/src/components/PracticeSchedule.vue
@@ -1,58 +1,27 @@
 <template>
-  <div class="container practice">
-    <h1 class="text-left">График занятий</h1>
+  <div class="calendar__schedule">
+    <div class="calendar__day-of-week">
+      <div>Понедельник</div>
+      <div>Вторник</div>
+      <div>Среда</div>
+      <div>Четверг</div>
+      <div>Пятница</div>
+      <div>Суббота</div>
+      <div>Воскресенье</div>
+    </div>
 
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">Пн</th>
-          <th scope="col">Вт</th>
-          <th scope="col">Ср</th>
-          <th scope="col">Чт</th>
-          <th scope="col">Пт</th>
-          <th scope="col">Сб</th>
-          <th scope="col">Вс</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th>27</th>
-          <td>28</td>
-          <td>29</td>
-          <td>30</td>
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-        </tr>
-        <tr>
-          <th scope="row">4</th>
-          <td>5</td>
-          <td>6</td>
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
-          <td>10</td>
-        </tr>
-        <tr>
-          <th>11</th>
-          <td>12</td>
-          <td>13</td>
-          <td>14</td>
-          <td>15</td>
-          <td>16</td>
-          <td>17</td>
-        </tr>
-        <tr>
-          <th>18</th>
-          <td>19</td>
-          <td>20</td>
-          <td>21</td>
-          <td>22</td>
-          <td>23</td>
-          <td>24</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="calendar__date-grid">
+      <button class="calendar__cell">1</button>
+      <button class="calendar__cell">2</button>
+      <button class="calendar__cell">3</button>
+      <button class="calendar__cell">4</button>
+      <button class="calendar__cell">5</button>
+      <button class="calendar__cell">6</button>
+      <button class="calendar__cell">7</button>
+      <button class="calendar__cell">8</button>
+      <button class="calendar__cell">9</button>
+      <button class="calendar__cell">10</button>
+    </div>
   </div>
 </template>
 
--- admin-app/src/views/Calendar.vue
+++ admin-app/src/views/Calendar.vue
@@ -21,31 +21,6 @@
       <div class="actions__date">20 Май 2020</div>
     </div>
 
-    <div class="calendar__schedule">
-      <div class="calendar__day-of-week">
-        <div>Понедельник</div>
-        <div>Вторник</div>
-        <div>Среда</div>
-        <div>Четверг</div>
-        <div>Пятница</div>
-        <div>Суббота</div>
-        <div>Воскресенье</div>
-      </div>
-
-      <div class="calendar__date-grid">
-        <button class="calendar__cell">1</button>
-        <button class="calendar__cell">2</button>
-        <button class="calendar__cell">3</button>
-        <button class="calendar__cell">4</button>
-        <button class="calendar__cell">5</button>
-        <button class="calendar__cell">6</button>
-        <button class="calendar__cell">7</button>
-        <button class="calendar__cell">8</button>
-        <button class="calendar__cell">9</button>
-        <button class="calendar__cell">10</button>
-      </div>
-    </div>
-
     <practice-schedule msg="msg" />
     {{ practice.email }}
   </div>
