ca676ab7d5e4f90a0f12f698184baf24c6f3ea70
superpuper32 <pochitalin_m@mail.ru>
2020-06-14 11:42:42 +0300

    profile page added styles & fixed

--- admin-app/src/components/Profile/index.vue
+++ admin-app/src/components/Profile/index.vue
@@ -6,11 +6,25 @@
 
     <div class="profile__header">
       <div class="profile__info">
+        <div class="profile__avatar">
+          <div class="profile__img">
+            <img
+              src="https://www.gravatar.com/avatar/e3c4bc11e6b5852d8709c907104ef048?s=140&d=retro"
+              alt="avatar"
+            />
+          </div>
+        </div>
         <div class="profile__name">User Name</div>
         <div class="profile__position">Должность</div>
-        <div
-          class="profile__description"
-        >Значимость этих проблем настолько очевидна, что начало повседневной работы по формированию позиции играет важную роль в формировании модели развития</div>
+        <div class="profile__description">
+          Значимость этих проблем настолько очевидна, что начало повседневной работы по формированию
+          позиции играет важную роль в формировании модели развития
+        </div>
+
+        <div class="profile__btns">
+          <div class="profile__btn">1 матч</div>
+          <div class="profile__btn">1 матч</div>
+        </div>
       </div>
 
       <div class="profile__payment">
