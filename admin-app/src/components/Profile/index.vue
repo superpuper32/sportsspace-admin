cd54a2592cf269ec15c42f16dd47a1b101f3d282
superpuper32 <pochitalin_m@mail.ru>
2020-06-14 14:57:35 +0300

    profile page info header finished

--- admin-app/src/components/Profile/index.vue
+++ admin-app/src/components/Profile/index.vue
@@ -6,24 +6,30 @@
 
     <div class="profile__header">
       <div class="profile__info">
-        <div class="profile__avatar">
-          <div class="profile__img">
-            <img
-              src="https://www.gravatar.com/avatar/e3c4bc11e6b5852d8709c907104ef048?s=140&d=retro"
-              alt="avatar"
-            />
+        <div>
+          <div class="profile__avatar">
+            <div class="profile__img">
+              <img
+                src="https://www.gravatar.com/avatar/e3c4bc11e6b5852d8709c907104ef048?s=140&d=retro"
+                alt="avatar"
+              />
+            </div>
           </div>
-        </div>
-        <div class="profile__name">User Name</div>
-        <div class="profile__position">Должность</div>
-        <div class="profile__description">
-          Значимость этих проблем настолько очевидна, что начало повседневной работы по формированию
-          позиции играет важную роль в формировании модели развития
+
+          <div class="profile__name">User Name</div>
+
+          <div class="profile__position">Должность</div>
+
+          <div class="profile__description">
+            Значимость этих проблем настолько очевидна, что начало повседневной работы по
+            формированию позиции играет важную роль в формировании модели развития
+          </div>
         </div>
 
         <div class="profile__btns">
-          <div class="profile__btn">1 матч</div>
-          <div class="profile__btn">1 матч</div>
+          <div class="profile__btn">1 Матч</div>
+
+          <div class="profile__btn">1 Матч</div>
         </div>
       </div>
 
@@ -32,11 +38,20 @@
           <span>500 ₽</span>
         </div>
 
-        <div class="profile__links">
-          <ul>
-            <li>Google</li>
-            <li>Instagram</li>
-            <li>facebook</li>
+        <div class="profile__social">
+          <ul class="profile__links">
+            <li data-social="google">
+              <a href="#" class="social-google"></a>
+            </li>
+            <li data-social="instagram">
+              <a href="#" class="social-instagram"></a>
+            </li>
+            <li data-social="facebook">
+              <a href="#" class="social-facebook"></a>
+            </li>
+            <li data-social="new">
+              <a href="#" class="social-new"></a>
+            </li>
           </ul>
         </div>
       </div>
