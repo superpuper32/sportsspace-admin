5e4db5c132fcf16f66be206de2c5debab7b5b019
superpuper32 <pochitalin_m@mail.ru>
2020-06-17 20:06:06 +0300

    toggle button added

---
+++ admin-app/src/components/SettingsNotification/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="settings-notification">
+    <div class="settings-notification__info">
+      <span class="settings-notification__title">Обновления</span>
+      <div class="settings-notification__description">MasterCard ending in 4078, exp: 08/2020</div>
+    </div>
+
+    <label class="toggle-button toggled" arial-label="Unsubscribe" for="Unsubscribe">
+      <input type="checkbox" class="toggle-button__input" id="Unsubscribe" checked />
+      <span class="toggle-button__span">Unsubscribe From Everything</span>
+      <div class="toggle-button__track"></div>
+      <div class="toggle-button__thumb"></div>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SettingsNotification'
+}
+</script>
+
+<style lang="sass" scoped>
+.settings-notification
+    margin-right: 26px
+    margin-bottom: 30px
+    display: flex
+    justify-content: space-between
+    align-items: center
+    font-style: normal
+    font-weight: normal
+
+    &__title
+        font-size: 17px
+        line-height: 22px
+        color: #2C2E3F
+
+    &__description
+        font-size: 14px
+        line-height: 21px
+        color: #485C73
+
+.toggle-button
+    display: inline-block
+    position: relative
+    cursor: pointer
+    background-color: initial
+    border: 0
+    padding: 0
+    outline: none
+
+    &__input
+        position: absolute
+        width: 1px
+        height: 1px
+        padding: 0
+        overflow: hidden
+        clip: rect(0,0,0,0)
+        white-space: nowrap
+        -webkit-clip-path: inset(50%)
+        clip-path: inset(50%)
+        border: 0
+
+    &__span
+        position: absolute
+        width: 1px
+        height: 1px
+        padding: 0
+        overflow: hidden
+        clip: rect(0,0,0,0)
+        white-space: nowrap
+        -webkit-clip-path: inset(50%)
+        clip-path: inset(50%)
+        border: 0
+
+    &__track
+        width: 40px
+        height: 22px
+        padding: 0
+        border-radius: 12px
+        background-color: #F8FAFB
+        border: 1px solid #E8ECEF
+        transition: all .2s ease
+
+    &__thumb
+        position: absolute
+        top: 3px
+        left: 5px
+        width: 16px
+        height: 16px
+        border-radius: 12px
+        transition: all .5s cubic-bezier(.23,1,.32,1)
+        background-color: #98A9BC
+
+.toggled
+
+    .toggle-button__track
+        background-color: #008dff
+
+    .toggle-button__thumb
+        background-color: #fff
+        left: 20px
+</style>
--- admin-app/src/views/settings/notification/index.vue
+++ admin-app/src/views/settings/notification/index.vue
@@ -2,24 +2,14 @@
   <div>
     <h3>Уведомления</h3>
 
-    <form class="settings__form">
-      <div class="settings__input">
-        <label for="name">ФИО</label>
-        <input type="text" name="name" placeholder="Иванов" />
-      </div>
+    <settings-notification />
+    <settings-notification />
+    <settings-notification />
 
-      <div class="settings__input">
-        <label for="email">Должность</label>
-        <input type="email" name="email" placeholder="Введите email" />
-      </div>
-
-      <div class="settings__input">
-        <label for="position">Должность</label>
-        <input type="text" name="position" placeholder="Тренер" />
-      </div>
-    </form>
-
-    <button class="main__button">Обновить</button>
+    <div class="settings__btns">
+      <button class="button button__resting">Отмена</button>
+      <button class="button button__main">Обновить</button>
+    </div>
   </div>
 </template>
 
@@ -31,6 +21,9 @@ export default {
       type: Object,
       required: true
     }
+  },
+  components: {
+    SettingsNotification: () => import('@/components/SettingsNotification')
   }
 }
 </script>
