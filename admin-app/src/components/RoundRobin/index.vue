3822116b614bc854fb7f7235ecc6a82278111281
superpuper32 <pochitalin_m@mail.ru>
2020-07-31 19:10:53 +0300

    tournaments round-robin table component added

---
+++ admin-app/src/components/RoundRobin/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="round-robin">Распределение команд рандомно по таблице</div>
+</template>
+
+<script>
+export default {
+  name: 'RoundRobin'
+}
+</script>
+
+<style lang="sass" scoped>
+.round-robin
+    color: #2C2E3F
+</style>
--- admin-app/src/views/calendar/tournaments/ManageTournament.vue
+++ admin-app/src/views/calendar/tournaments/ManageTournament.vue
@@ -8,87 +8,7 @@
       </li>
     </ul>
 
-    <div class="single-tour">
-      <div class="single-tour__round">
-        <div class="single-tour__play">
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_green">1</div>
-          </div>
-          <div class="single-tour__score">0 : 0</div>
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_green">1</div>
-          </div>
-        </div>
-
-        <div class="single-tour__play">
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_red">2</div>
-          </div>
-          <div class="single-tour__score">0 : 0</div>
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_red">2</div>
-          </div>
-        </div>
-
-        <div class="single-tour__play">
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_orange">3</div>
-          </div>
-          <div class="single-tour__score">0 : 0</div>
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_orange">3</div>
-          </div>
-        </div>
-
-        <div class="single-tour__play">
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_yellow">4</div>
-          </div>
-          <div class="single-tour__score">0 : 0</div>
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_yellow">4</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="single-tour__round">
-        <div class="single-tour__play">
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_pink">5</div>
-          </div>
-          <div class="single-tour__score">0 : 0</div>
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_pink">5</div>
-          </div>
-        </div>
-
-        <div class="single-tour__play">
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_violet">6</div>
-          </div>
-          <div class="single-tour__score">0 : 0</div>
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_violet">6</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="single-tour__round">
-        <div class="single-tour__play">
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_pink">7</div>
-          </div>
-          <div class="single-tour__score">0 : 0</div>
-          <div class="single-tour__team">
-            <div class="single-tour__number single-tour__number_pink">7</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="single-tour__winner">
-        <span>Команда</span>
-      </div>
-    </div>
+    <round-robin />
 
     <tournament-panel />
   </section>
@@ -98,7 +18,9 @@
 export default {
   name: 'ManageTournament',
   components: {
-    TournamentPanel: () => import('@/components/TournamentPanel')
+    TournamentPanel: () => import('@/components/TournamentPanel'),
+    // SingleIllumination: () => import('@/components/SingleIllumination'),
+    RoundRobin: () => import('@/components/RoundRobin')
   },
   data: () => ({
     teams: [
@@ -118,138 +40,36 @@ export default {
 <style lang="sass" scoped>
 .manage
 
-    h2
-        margin-bottom: 24px
+  h2
+    margin-bottom: 24px
 
-    &__teams
-        margin-right: 320px
-        margin-bottom: 50px
-        padding-left: 0px
-        display: flex
-        flex-wrap: wrap
-
-    &__team
-        margin: 0px 24px 24px 0px
-        min-width: 200px
-        // border: 1.5px dashed #BFD4E4
-        border-radius: 6px
-        list-style: none
-
-        span
-            box-sizing: border-box
-            display: block
-            padding: 12px 10px 12px 8px
-            width: 100%
-            font-family: SF Pro Text
-            font-style: normal
-            font-weight: 600
-            font-size: 17px
-            line-height: 22px
-            color: #485C73
-            background: #FFFFFF
-            border: 1.5px solid #BFD4E4
-            border-radius: 6px
-            cursor: grab
-
-.single-tour
+  &__teams
+    margin-right: 320px
+    margin-bottom: 50px
+    padding-left: 0px
     display: flex
+    flex-wrap: wrap
 
-    &__round
-        display: flex
-        flex-direction: column
-        justify-content: space-around
+  &__team
+    margin: 0px 24px 24px 0px
+    min-width: 200px
+      // border: 1.5px dashed #BFD4E4
+    border-radius: 6px
+    list-style: none
 
-    &__play
-        margin-right: 40px
-        margin-bottom: 32px
-
-    &__team
-        padding: 8px
-        display: flex
-        align-items: center
-        width: 166px
-        height: 46px
-        background: #FFFFFF
-        border: 1.5px dashed #BFD4E4
-        box-sizing: border-box
-        border-radius: 6px
-
-    &__score
-        padding: 7px
-        width: 166px
-        height: 32px
-        text-align: center
-        font-family: SF Pro Text
-        font-style: normal
-        font-weight: 600
-        font-size: 15px
-        line-height: 20px
-        color: #0D9372
-
-    &__number
-        display: inline-block
-        margin-right: 10px
-        padding: 4px 10px
-        font-family: SF Pro Display
-        font-style: normal
-        font-weight: bold
-        font-size: 10px
-        line-height: 16px
-        letter-spacing: -0.3px
-        border-radius: 6px
-        color: #FFFFFF
-
-        &_green
-            background: #66C109
-            border: 1.5px solid rgba(78, 154, 0, 0.7)
-
-        &_red
-            background: #F64662
-            border: 1.5px solid rgba(228, 46, 75, 0.7)
-
-        &_orange
-            background: #FF905F
-            border: 1.5px solid rgba(255, 81, 35, 0.7)
-
-        &_yellow
-            background: #FFCA32
-            border: 1.5px solid rgba(255, 152, 0, 0.7)
-
-        &_violet
-            background: #9B8AE6
-            border: 1.5px solid rgba(94, 74, 207, 0.7)
-
-        &_pink
-            background: #EC7FD3
-            border: 1.5px solid rgba(218, 63, 174, 0.7)
-
-    &__winner
-        margin-bottom: 32px
-        display: flex
-        align-items: center
-
-        span
-            padding: 18px 12px 18px 63px
-            position: relative
-            background: #E8ECEF
-            border: 1.5px solid #BFD4E4
-            box-sizing: border-box
-            border-radius: 6px
-            font-family: SF Pro Text
-            font-style: normal
-            font-weight: 600
-            font-size: 17px
-            line-height: 22px
-            text-align: center
-            color: #BFD4E4
-
-            &:before
-                content: ''
-                position: absolute
-                top: 50%
-                left: 12px
-                width: 41px
-                height: 35px
-                background: url(../../../assets/winner-volleyball.svg) center center no-repeat
-                transform: translateY(-50%)
+    span
+      box-sizing: border-box
+      display: block
+      padding: 12px 10px 12px 8px
+      width: 100%
+      font-family: SF Pro Text
+      font-style: normal
+      font-weight: 600
+      font-size: 17px
+      line-height: 22px
+      color: #485C73
+      background: #FFFFFF
+      border: 1.5px solid #BFD4E4
+      border-radius: 6px
+      cursor: grab
 </style>
